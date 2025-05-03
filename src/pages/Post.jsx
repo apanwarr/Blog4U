e23d8266ea6bc4ab55836a25e5e9602c3e86b968
Ashish Panwar <150328781+apanwarr@users.noreply.github.com>
2025-05-03 15:22:03 +0530

    Update Post.jsx

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -33,9 +33,9 @@ export default function Post() {
     };
 
     return post ? (
-        <div className="py-8 mt-4">
+        <div className="py-8  mt-4 mb-4 relative border rounded-xl p-2">
             <Container>
-                <div className="w-full flex justify-center  mb-4 relative border rounded-xl p-2">
+                <div className="w-full flex justify-center ">
                     <img
                         src={appwriteService.getFilePreview(post.featuredImage)}
                         alt={post.title}
