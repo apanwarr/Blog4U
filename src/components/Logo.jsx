636d6af8669d8053c5489440f0c6a236d219d422
Ashish Panwar <150328781+apanwarr@users.noreply.github.com>
2025-05-04 11:24:14 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import blogIT from '../assets/blog_pic.jpg'
+import blogPic from '../assets/blog_pic.jpg'
 
 function Logo({width = '100px'}) {
   return (
     <div className='flex align-middle'>
-      <img className='h-10 rounded-md' src={blogIT} alt="" />
+      <img className='h-10 rounded-md' src={blogPic} alt="" />
       <span className='py-1 text-xl font-bold px-2'>Blog4U</span>
     </div>
   )
