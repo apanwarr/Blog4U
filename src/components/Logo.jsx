6a5a630122fa1d754de3d876e846850161328bf7
Ashish Panwar <150328781+apanwarr@users.noreply.github.com>
2025-05-04 11:22:57 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ function Logo({width = '100px'}) {
   return (
     <div className='flex align-middle'>
       <img className='h-10 rounded-md' src={blogIT} alt="" />
-      <span className='py-1 text-xl font-bold px-2'>blogIT</span>
+      <span className='py-1 text-xl font-bold px-2'>Blog4U</span>
     </div>
   )
 }
